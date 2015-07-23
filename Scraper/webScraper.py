f119c6367a516b6fb1e6a5c5d9ed026626d6da61
Kyle <Kyle.rogers533@gmail.com>
2015-07-23 11:54:51 -0400

    Removing useless code and adding page parsing functions to see if it will pull the right information

--- Scraper/webScraper.py
+++ Scraper/webScraper.py
@@ -1,26 +1,22 @@
-import requests
-import json
 from bs4 import BeautifulSoup
+import urllib.request
 
 # Save site url in a variable
-URL_Root = "http://wiki.teamliquid.net/"
-DEFAULT_DEST = "/smash//Frame_Data"
-CHARACTER_LIST = ['Fox','Falco','Sheik','Marth','Jigglypuff','Peach','Captain_Falcon','Luigi','Roy']
+URL = "http://smashboards.com/threads/captain-falcon-hitboxes-and-frame-data.284165/"
 
-# Pull the site into the smashBoards variable
-smashBoards = requests.get("")
+# Open the url and save the html data into bs4
+def request_page(link):
+    f = urllib.request.urlopen(link)
+    data = f.read()
+    webpage = BeautifulSoup(data)
+    return webpage
 
-# Print the status code to verify it worked
-print("Status code: " + smashBoards.status_code)
+def parse_info(webpage):
+    article_tag = webpage.find_all('article')[1]
+    print(article_tag.contents)
 
-# Store the page content for parsing
-pgContent = smashBoards.content
-
-# Get the raw html using BeautifulSoup
-soup = BeautifulSoup(pgContent, "html.parser")
-
-# Get information between first two article tags
-soup.find_all("article",limit=1)
+page = request_page(URL)
+parse_info(page)
 
 # Creates a file to hold the data
 # dataFile = open(input("Enter the name of the file: ", "w"))
