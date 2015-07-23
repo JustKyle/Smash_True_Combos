fe98bcdf007a81bb480c7997fbff644dd2c4fc18
Kyle <Kyle.rogers533@gmail.com>
2015-07-23 13:51:10 -0400

    Removing useless code again, switched to requests instead of Urllib

--- Scraper/webScraper.py
+++ Scraper/webScraper.py
@@ -1,19 +1,18 @@
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 
 # Save site url in a variable
 URL = "http://smashboards.com/threads/captain-falcon-hitboxes-and-frame-data.284165/"
 
 # Open the url and save the html data into bs4
 def request_page(link):
-    f = urllib.request.urlopen(link)
-    data = f.read()
-    webpage = BeautifulSoup(data)
+    r = requests.get(link)
+    data = r.text
+    webpage = BeautifulSoup(data, 'html.parser')
     return webpage
 
 def parse_info(webpage):
-    article_tag = webpage.find_all('article',limit = 1)
-    print(article_tag.contents)
+    print(webpage.article.text)
 
 page = request_page(URL)
 parse_info(page)
